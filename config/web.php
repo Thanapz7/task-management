83f62e57e65b4a8d99072f3f8c329cb9afd8f586
nukun56788 <107339530+nukun56788@users.noreply.github.com>
2025-01-21 15:31:27 +0700

    add fields

--- config/web.php
+++ config/web.php
@@ -15,6 +15,18 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'CnQjEz6BxyA-enFjbWLonzN3s0xuEz5T',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
+        ],
+        'corsFilter' => [
+            'class' => 'yii\filters\Cors::class',
+            'cors' => [
+//                'Origin' => ['*'],
+//                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//                'Access-Control-Allow-Credentials' => true,
+                'class' => \yii\filters\Cors::class,
+            ],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -48,6 +60,7 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+//                'home/fields/<id:\d+>' => 'home/fields',
 //                // กฎการกำหนด URL ที่เป็นมิตร
 //                'user/update/<id:\d+>' => 'user/update',
 ////                'user/view/<id:\d+>' => 'user/view',
